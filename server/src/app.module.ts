1f4342222e969fb5f65da4decb8e8fbed90b7307
Swedka121 <ivan.hryshchuk2011@gmail.com>
2026-02-18 21:26:25 +0200

    feat: Added typeorm and config modules to project, set up configuration for database and typeorm module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthorizationModule } from './authorization/authorization.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeormDatasource } from './configs/Database.config';
+import DatabaseConfig from './configs/Database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthorizationModule],
+  imports: [
+    AuthorizationModule,
+    ConfigModule.forRoot({
+      load: [TypeormDatasource, DatabaseConfig],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(TypeormDatasource()),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ server/src/configs/Database.config.ts
@@ -0,0 +1,31 @@
+import { registerAs } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+interface DatabaseConfig {
+  password: string;
+  user: string;
+  name: string;
+  port: number;
+  version: number;
+}
+
+export default registerAs(
+  'database',
+  (): DatabaseConfig => ({
+    name: process.env.DB_NAME ?? 'CinaGloria',
+    password: process.env.DB_PASSWORD ?? 'admin',
+    user: process.env.DB_USER ?? 'admin',
+    port: Number(process.env.DB_PORT ?? '5432'),
+    version: Number(process.env.DB_VERSION ?? '18'),
+  }),
+);
+
+export const TypeormDatasource = (): TypeOrmModuleOptions => ({
+  type: 'postgres',
+  port: Number(process.env.DB_PORT ?? '5432'),
+  host: 'db',
+  username: process.env.DB_USER ?? 'admin',
+  password: process.env.DB_PASSWORD ?? 'admin',
+  database: process.env.DB_NAME ?? 'CinaGloria',
+  synchronize: process.env.NODE_ENV != 'PRODUCTION',
+});

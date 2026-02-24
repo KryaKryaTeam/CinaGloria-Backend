9fd25595a81de6fb943097bf7f0bafd12d074065
Swedka121 <ivan.hryshchuk2011@gmail.com>
2026-02-24 19:54:17 +0200

    feat: Add GitHub Actions workflow for building and pushing Docker images, update Dockerfile for multi-stage build, and enhance cookie and database configurations

--- server/src/configs/Cookie.config.ts
+++ server/src/configs/Cookie.config.ts
@@ -6,12 +6,13 @@ export default registerAs(
   (): CookieOptions => ({
     httpOnly: true,
     secure:
-      process.env.NODE_ENV == 'PRODUCTION' || process.env.IS_PREVIEW == 'TRUE',
+      process.env.NODE_ENV?.toUpperCase() == 'PRODUCTION' ||
+      process.env.IS_PREVIEW == 'TRUE',
     expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),
     sameSite:
       process.env.IS_PREVIEW == 'TRUE'
         ? 'none'
-        : process.env.NODE_ENV == 'PRODUCTION'
+        : process.env.NODE_ENV?.toUpperCase() == 'PRODUCTION'
           ? 'strict'
           : 'lax',
     path: '/',

3f794543d8754311b076be41c4d48c3a1de1d2f6
Swedka121 <ivan.hryshchuk2011@gmail.com>
2026-02-25 21:41:27 +0200

    feat: set up documentation endpoint with Swagger and add new dependencies

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,14 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableVersioning({ type: VersioningType.URI });
   app.enableCors({
     origin: process.env.ALLOWED_ORIGIN ?? 'http://localhost:3000',
     credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('My API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addServer('https://bots.swedka121.com/')
+    .build();
+  const doc = SwaggerModule.createDocument(app, config);
+  app.use('/docs', apiReference({ content: doc }));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

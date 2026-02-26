87cd9d123f2dbc4fa222b9fe8814f8b784a79a09
Swedka121 <ivan.hryshchuk2011@gmail.com>
2026-02-26 21:46:01 +0200

    feat: implement Notification domain entity

---
+++ server/src/notification/domain/entities/Notification.ts
@@ -0,0 +1,67 @@
+import { randomUUID } from 'crypto';
+import { UserEntity } from 'src/authorization/domain/entities/User.entity';
+import { DomainError, DomainErrors } from 'src/error/DomainError';
+import { NotificationStatus } from 'src/types/NotificationStatus';
+
+interface INotification {
+  id: string;
+  title: string;
+  content: string;
+  from: string;
+  to: UserEntity;
+  status: NotificationStatus;
+  targets: string[];
+}
+
+export class Notification {
+  public readonly id: string;
+  public readonly title: string;
+  public readonly content: string;
+  public readonly from: string;
+  public readonly to: UserEntity;
+  private _status: NotificationStatus;
+  public readonly targets: string[];
+
+  private constructor(partial: Partial<Notification>) {
+    Object.assign(this, partial);
+  }
+
+  public static create(
+    obj: {
+      title: string;
+      content: string;
+      from: string;
+      to: UserEntity;
+      targets: string[];
+    },
+    avalibleProviders: string[],
+  ) {
+    if (obj.title.length < 5 || obj.title.length > 100)
+      throw new DomainError(DomainErrors.UNEXPECTED_VALUE);
+    if (!obj.targets.every((val) => avalibleProviders.includes(val)))
+      throw new DomainError(DomainErrors.UNEXPECTED_VALUE);
+
+    return new Notification({
+      id: randomUUID(),
+      status: NotificationStatus.sended,
+      ...obj,
+    });
+  }
+
+  public static load(obj: INotification) {
+    return new Notification(obj);
+  }
+
+  get status() {
+    return this._status;
+  }
+
+  set status(NextStatus: NotificationStatus) {
+    if (this._status == NotificationStatus.readed)
+      throw new DomainError(DomainErrors.UNEXPECTED_VALUE);
+    if (this._status == NextStatus)
+      throw new DomainError(DomainErrors.NO_CHANGE);
+
+    this._status = NextStatus;
+  }
+}

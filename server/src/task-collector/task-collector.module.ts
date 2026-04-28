92e66df6a8fcd838630bb61eb32490b893cd9656
lll <hamacok46@gmail.com>
2026-04-28 22:34:29 +0300

    feat: Implement submition repo, entity, mapper

--- server/src/task-collector/task-collector.module.ts
+++ server/src/task-collector/task-collector.module.ts
@@ -1,9 +1,11 @@
 import { Module, Provider } from '@nestjs/common';
 import { MapperTokens } from 'src/common/Tokens';
 import { CriteriaMapper } from './application/mappers/CriteriaMapper';
+import { SubmitionMapper } from './application/mappers/SubmitionMapper';
 
 const providers: Provider[] = [
   { provide: MapperTokens.CriteriaMapper, useClass: CriteriaMapper },
+  { provide: MapperTokens.SubmitionMapper, useClass: SubmitionMapper },
 ];
 
 @Module({
